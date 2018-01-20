43a25f5bfc10d75fa1f004df551ce8c1c4347ab9
Alex Conrey <alex@conrey.us>
2018-01-20 17:45:20 -0600

    fixed ssh known hosts key checking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SSH_OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
 if [ -f .deploy_rsa ]; then
 	chmod 600 .deploy_rsa
 	echo "Pulling latest ${TRAVIS_BRANCH} on remote host"
-	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "touch `date +'%Y%m%d%H%M%S'`"
-	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && git pull"
+	ssh ${SSH_OPTIONS} -l zoidberg -i .deploy_rsa 18.218.12.219 "touch `date +'%Y%m%d%H%M%S'`"
+	ssh ${SSH_OPTIONS} -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && git pull"
 	echo "Installing new npm pkgs on remote host"
-	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && npm install"
+	ssh ${SSH_OPTIONS} -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && npm install"
 	echo "Restarting zoidberg service on remote host"
-	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "sudo service zoidberg stop; sleep 1; sudo service zoidberg start" 
+	ssh ${SSH_OPTIONS} -l zoidberg -i .deploy_rsa 18.218.12.219 "sudo service zoidberg stop; sleep 1; sudo service zoidberg start" 
 fi
