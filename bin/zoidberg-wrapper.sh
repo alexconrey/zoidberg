435e8614f9cab948ad47fc1e278427a14397c380
Alex Conrey <alex@conrey.us>
2018-01-20 14:37:05 -0600

    updated zoidberg wrapper

--- bin/zoidberg-wrapper.sh
+++ bin/zoidberg-wrapper.sh
@@ -2,11 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export HUBOT_SLACK_TOKEN="$1"
+export HUBOT_SLACK_TOKEN=$(cat $DIR/../.slack_creds)
 export HUBOT_LOG_LEVEL="info"
 
 cd $DIR/..
-if [ ! -d $DIR/../logs ]; then
-	mkdir $DIR/../logs
+if [ ! -d ./logs ]; then
+	mkdir ./logs
 fi
+
+if [ -f ./logs/zoidberg.log ]; then
+	mv ./logs/zoidberg.log ./logs/zoidberg-$(date +'%Y%m%d%H%M%S').log
+fi
+
 ./bin/hubot --adapter slack > logs/zoidberg.log 2>&1
