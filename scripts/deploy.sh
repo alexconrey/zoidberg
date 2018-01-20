3640825a35e4df89d9f30605f81cb64560741e9a
Alex Conrey <alex@conrey.us>
2018-01-20 17:24:22 -0600

    hoping this is a bit better

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,11 @@
 
 if [ -f .deploy_rsa ]; then
 	chmod 600 .deploy_rsa
-	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && git pull && npm install && sudo service zoidberg restart" 
+	echo "Pulling latest ${TRAVIS_BRANCH} on remote host"
+	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "touch `date +'%Y%m%d%H%M%S'`"
+	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && git pull"
+	echo "Installing new npm pkgs on remote host"
+	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "cd /opt/zoidberg && npm install"
+	echo "Restarting zoidberg service on remote host"
+	ssh -l zoidberg -i .deploy_rsa 18.218.12.219 "sudo service zoidberg stop; sleep 1; sudo service zoidberg start" 
 fi
